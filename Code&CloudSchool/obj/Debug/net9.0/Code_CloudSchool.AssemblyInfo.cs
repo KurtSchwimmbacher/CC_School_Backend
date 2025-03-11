50401fb91097d2b68febfed6af4afbf4e5e6a23a
KurtSchwimmbacher <231002@virtualwindow.co.za>
2025-03-11 11:37:01 +0200

    chore(im not sure what these are) -- these just came up

--- Code&CloudSchool/obj/Debug/net9.0/Code_CloudSchool.AssemblyInfo.cs
+++ Code&CloudSchool/obj/Debug/net9.0/Code_CloudSchool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4529dcc634c123fad09082918a0ce5d706f86a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+470a93629ae771179cc045ed2ab6d93607508ff6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
