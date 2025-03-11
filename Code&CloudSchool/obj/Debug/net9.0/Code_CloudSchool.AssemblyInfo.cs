2d7d53c6545de33fb168db98db9f7ccb0f53b5cd
KurtSchwimmbacher <231002@virtualwindow.co.za>
2025-03-11 12:27:04 +0200

    chore(install dependencies) -- install microsoft dependencies

--- Code&CloudSchool/obj/Debug/net9.0/Code_CloudSchool.AssemblyInfo.cs
+++ Code&CloudSchool/obj/Debug/net9.0/Code_CloudSchool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b535d1a4b693dd8710540a54a5e06877bfaa6eac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+637ccd614b8a8f1710b3c116ff94c10b2e442349")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
