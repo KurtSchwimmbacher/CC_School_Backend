5ad0b82ce12439f35151299dce7c1a55ac6b3cad
KurtSchwimmbacher <231002@virtualwindow.co.za>
2025-03-13 16:34:02 +0200

    chore(migrationi) -- create initial migration

--- Code&CloudSchool/obj/Debug/net9.0/Code_CloudSchool.AssemblyInfo.cs
+++ Code&CloudSchool/obj/Debug/net9.0/Code_CloudSchool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e948e9ba2e66921e2f8fdee9f5d0c3c5f3f37059")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6300383f6f3981d818cfae33c61c5ec30811493")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
