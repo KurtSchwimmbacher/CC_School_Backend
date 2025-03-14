9e8e32a7a212140d5d27fae3db9aef16e7a07f96
Lebogang Hlongwane 231214 <231214@virtualwindow.co.za>
2025-03-14 11:32:46 +0200

    Latest Commit
    completed the Lecturers Registration class and started with the Lecturers API controller

---
+++ Code&CloudSchool/Models/LecturerReg.cs
@@ -0,0 +1,39 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Code_CloudSchool.Models;
+
+public class LecturerReg
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+    public int Id { get;set;}
+
+    [Required]
+    [StringLength (50)]
+    public string LectName {get; set;} = string.Empty;
+
+    [Required]
+    [MaxLength(50)]
+    public string LecLastName {get;set;} =string.Empty;
+
+    [Required]
+    [EmailAddress]
+    [MaxLength(150)]
+    public string Email {get;set;} = string.Empty;
+
+    [Required]
+    [MaxLength(50)]
+    public string PhoneNumber {get;set;} = string.Empty;
+    
+    [Required]
+    public string Department {get;set;} = string.Empty;
+
+    [Required]
+    public DateTime DateOfJoining {get;set;} 
+
+    public bool IsActive {get;set;} = true;
+
+}
