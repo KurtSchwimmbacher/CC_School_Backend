2432d640a5dc1729a177aa80fcb687617cd6527c
Lebogang Hlongwane 231214 <231214@virtualwindow.co.za>
2025-03-14 09:58:27 +0200

    Stashed Changes commit

--- Code&CloudSchool/obj/Debug/net9.0/Code&CloudSchool.AssemblyInfo.cs
+++ Code&CloudSchool/obj/Debug/net9.0/Code&CloudSchool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code&CloudSchool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59cd4b1ba975ba13bd738eea6a7fceb5001beb6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c73c0ebedf881f0d2e0a179edb5cea153d628124")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code&CloudSchool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code&CloudSchool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
