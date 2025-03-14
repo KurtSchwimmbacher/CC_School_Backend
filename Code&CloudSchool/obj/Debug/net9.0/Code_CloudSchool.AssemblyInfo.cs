7534dfb202a01f29c51114952f3af642ffd6cdc5
Lebogang Hlongwane 231214 <231214@virtualwindow.co.za>
2025-03-14 12:55:26 +0200

    Lecturer Auth Service
    Just completed the Lecturer Auth service

--- Code&CloudSchool/obj/Debug/net9.0/Code_CloudSchool.AssemblyInfo.cs
+++ Code&CloudSchool/obj/Debug/net9.0/Code_CloudSchool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b51565260ae6772ebb34c64b80db1643b6d7b32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9af4cc18c339032324d904975c1f50ed30e1244f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
