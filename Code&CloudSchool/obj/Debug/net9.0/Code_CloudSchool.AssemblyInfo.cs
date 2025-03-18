6bfa1e0cc7120b8d827fd449c3a15ac937f67115
Lebogang Hlongwane 231214 <231214@virtualwindow.co.za>
2025-03-18 15:43:30 +0200

    First Commit

--- Code&CloudSchool/obj/Debug/net9.0/Code_CloudSchool.AssemblyInfo.cs
+++ Code&CloudSchool/obj/Debug/net9.0/Code_CloudSchool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d7d53c6545de33fb168db98db9f7ccb0f53b5cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6e607024162309887b5dd614c8267e756e4de8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
