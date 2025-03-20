f1f0900ba7ab04cb325738cca3614694d672e619
Lebogang Hlongwane 231214 <231214@virtualwindow.co.za>
2025-03-20 13:44:55 +0200

    stashed changes

--- Code&CloudSchool/obj/Debug/net9.0/Code_CloudSchool.AssemblyInfo.cs
+++ Code&CloudSchool/obj/Debug/net9.0/Code_CloudSchool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56e842a3fb60767a459efbcbdb7d3101bdb606c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08a182b2af6c2577610ebcfce44c81fd617a9aab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
