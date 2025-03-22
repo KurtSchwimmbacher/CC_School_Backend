d99c7a3c646991a0449bfe142055d9c7cb1e2527
Lebogang Hlongwane 231214 <231214@virtualwindow.co.za>
2025-03-22 09:03:31 +0200

    Encountering error in the program.cs where the data file is not being detected

--- Code&CloudSchool/obj/Debug/net9.0/Code_CloudSchool.AssemblyInfo.cs
+++ Code&CloudSchool/obj/Debug/net9.0/Code_CloudSchool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16de8e9f43b21b0158f09029d91749d010c0952f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6afcd0b984c29541766d8dee04f1d7841ca6c82a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
