9d31e9c52f7f012002536ba891e341d810c5cc3c
ngozionthewebs <231285@virtualwindow.co.za>
2025-03-27 13:54:13 +0200

    adding git ignore files

--- Code&CloudSchool/obj/Debug/net9.0/Code_CloudSchool.AssemblyInfo.cs
+++ Code&CloudSchool/obj/Debug/net9.0/Code_CloudSchool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7de24283124044d47186eb3fa9d915272c9789b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3518bdd6eef36ed80b5c9d56033f21cdcbec3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
