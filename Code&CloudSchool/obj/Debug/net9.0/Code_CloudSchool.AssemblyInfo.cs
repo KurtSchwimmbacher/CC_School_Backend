1b4ff80ae22d2662d94e6e93ad2ef0ce1855f76c
Lebogang Hlongwane 231214 <231214@virtualwindow.co.za>
2025-03-27 13:54:51 +0200

    Just created the gitignore file

--- Code&CloudSchool/obj/Debug/net9.0/Code_CloudSchool.AssemblyInfo.cs
+++ Code&CloudSchool/obj/Debug/net9.0/Code_CloudSchool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5fa18d759d2ec9a5d502a6ff97e1e9c6ed0192c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1467f7fcc5f0b599c1afb7fda3f9f8f3506cd762")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code_CloudSchool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
