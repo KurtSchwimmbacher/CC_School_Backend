71d3aa26c6c46b1cb2253de21d7774c6a6d52e0c
Lebogang Hlongwane 231214 <231214@virtualwindow.co.za>
2025-04-14 17:08:56 +0200

    Created the login model
    Created the login model to start with the 2fa

---
+++ Code&CloudSchool/Models/Login.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Code_CloudSchool.Models;
+
+public class Login
+{
+
+[Required]
+    [Phone]
+    public string PhoneNumber { get; set; }  // For SMS/WhatsApp OTP
+
+    [Required]
+    public string OTP { get; set; }          // 6-digit code from user
+
+    // Optional: For traditional username/password + 2FA
+    public string Email { get; set; }        
+    public string Password { get; set; }     
+
+}
+
+
