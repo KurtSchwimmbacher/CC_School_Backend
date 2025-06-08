3e98c144279b4229b9325d8171435f4242fcc10f
KurtSchwimmbacher <231002@virtualwindow.co.za>
2025-06-08 11:54:50 +0200

    feat(add timetable by student endpoint)

--- CodeCloudSchool/Controllers/TimetableController.cs
+++ CodeCloudSchool/Controllers/TimetableController.cs
@@ -75,12 +75,44 @@ namespace Code_CloudSchool.Controllers
                     Day = cls.TimeSlot.Day.ToString(),
                     StartTime = cls.TimeSlot.StartTime.ToString(@"hh\:mm"),
                     EndTime = cls.TimeSlot.EndTime.ToString(@"hh\:mm"),
-                    Students = cls.Student.Select(s => s.Name +" "+ s.LastName).ToList(),
-                    Lecturers = cls.Lecturers.Select(l => l.Name +" "+ l.LastName).ToList()
+                    Students = cls.Student.Select(s => s.Name + " " + s.LastName).ToList(),
+                    Lecturers = cls.Lecturers.Select(l => l.Name + " " + l.LastName).ToList()
                 })
                 .ToListAsync();
 
             return Ok(scheduled);
+        }
+
+
+        [HttpGet("student/{studentId}")]
+        public async Task<IActionResult> GetStudentTimetable(Guid studentId)
+        {
+            var student = await _context.Students
+                .Include(s => s.Classes)
+                    .ThenInclude(c => c.TimeSlot)
+                .Include(s => s.Classes)
+                    .ThenInclude(c => c.Lecturers)
+                .FirstOrDefaultAsync(s => s.Id == studentId);
+
+            if (student == null) return NotFound("Student not found");
+
+            var studentTimetable = student.Classes
+                .Where(c => c.TimeSlotId != null)
+                .Select(cls => new TimetableDTO
+                {
+                    ClassID = cls.classID,
+                    ClassName = cls.className,
+                    ClassDescription = cls.classDescription,
+                    TimeSlotId = cls.TimeSlotId.Value,
+                    Day = cls.TimeSlot.Day.ToString(),
+                    StartTime = cls.TimeSlot.StartTime.ToString(@"hh\:mm"),
+                    EndTime = cls.TimeSlot.EndTime.ToString(@"hh\:mm"),
+                    Students = cls.Student.Select(s => s.Name + " " + s.LastName).ToList(),
+                    Lecturers = cls.Lecturers.Select(l => l.Name + " " + l.LastName).ToList()
+                })
+                .ToList();
+
+            return Ok(studentTimetable);
         }
 
 
